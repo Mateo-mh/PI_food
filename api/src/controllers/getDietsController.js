7d72902cf3e11bb46c55f14a8edd6746abd0e32b
Mateo-mh <byronmh8@gmail.com>
2023-07-14 17:36:58 -0500

    todo ok, solo falla diets y hay que ver detalles de post

--- api/src/controllers/getDietsController.js
+++ api/src/controllers/getDietsController.js
@@ -1,38 +1,40 @@
-const axios = require("axios")
-const URL = "https://api.spoonacular.com/recipes/complexSearch?apiKey=960565680fa546fba11fe01bb8e38180&number=100&addRecipeInformation=true&number=100"
-const { Recipe, Diet } = require("../db")
-const { findAllEndPoint } = require("../../addEndPoint")
-const { Op } = require("sequelize")
-//Mis funciones 
-const { RecipesTransformer } = require("../Functions/mainFunction")
+const axios = require("axios");
+const { Diet } = require("../db");
+const { apiKey, dietaFlag } = require("../../addEndPoint");
 
+const getDietsController = async (req, res) => {
+  try {
+    let foundDietsBd = [];
+    let foundDietsApi = [];
+    const aux = await Diet.findAll();
 
-async function getDietsController(req, res) {
-    try {
-        let foundDietsBd = []
-        let foundDietsApi = [] 
-        let aux = await Diet.findAll()
-        if(aux.length == 0){
-            const {data} = await axios.get(URL)
+    if (aux.length === 0) {
+      const API_URL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&number=100${dietaFlag}`;
+      const { data } = await axios.get(API_URL);
 
-            for(const recipe of data.results){
-                if(recipe.vegetarian == true && !foundDietsApi.includes("vegetarian")) foundDietsApi.push("vegetarian")
-                if(recipe.vegan == true && !foundDietsApi.includes("vegan")) foundDietsApi.push("vegan")
-                if(recipe.glutenFree == true && !foundDietsApi.includes("gluten free")) foundDietsApi.push("gluten free")
-                
-                recipe.diets.forEach(element => {
-                    if(!foundDietsApi.includes(element)) foundDietsApi.push(element)
-                });
-            }
-            return res.status(200).json(foundDietsApi)
-        }
-        aux.forEach((elem)=> foundDietsBd.push(elem.name) )
-        return res.status(200).json(foundDietsBd)
+      const dietTypes = ["vegetarian", "vegan", "gluten free"];
+      for (const recipe of data.results) {
+        dietTypes.forEach(dietType => {
+          if (recipe[dietType] && !foundDietsApi.includes(dietType)) {
+            foundDietsApi.push(dietType);
+          }
+        });
 
-    } catch (error) {
-        return res.status(500).send(error.message)
+        recipe.diets.forEach(element => {
+          if (!foundDietsApi.includes(element)) {
+            foundDietsApi.push(element);
+          }
+        });
+      }
 
+      return res.status(200).json(foundDietsApi);
     }
 
-}
-module.exports = getDietsController
+    aux.forEach(elem => foundDietsBd.push(elem.name));
+    return res.status(200).json(foundDietsBd);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+
+module.exports = getDietsController;
--- api/src/controllers/getIdController.js
+++ api/src/controllers/getIdController.js
@@ -1,33 +1,30 @@
-//Importamos la tabla 
-const URL = "https://api.spoonacular.com/recipes/{id}/information"
-const { Recipe } = require("../db")
-const axios = require("axios")
-//importmaos cosas extra 
-const { apiKey, END_POINT_ERROR, dietaFlag } = require("../../addEndPoint")
-const { RecipesTransformer } = require("../Functions/mainFunction")
+const axios = require("axios");
+const { Recipe } = require("../db");
+const { RecipesTransformer } = require("../Functions/mainFunction");
+const { apiKey, dietaFlag } = require("../../addEndPoint");
 
 const getIdController = async (req, res) => {
-    //Utilizamos una expresion regular para indentificar id de tipo UUID
-    const uuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  const uuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
-    try {
-        const { id } = req.params
-        let obj = ""
-        if (uuidPattern.test(id)) { // LO utilizamos para que no de un error de el id 
-            obj = await Recipe.findOne({ where: { id } })
-        }
-        if (!obj) {
-            const { data } = await axios.get(`https://api.spoonacular.com/recipes/${id}/information${apiKey}${dietaFlag}`)
-            obj = RecipesTransformer(data)
+  try {
+    const { id } = req.params;
+    let obj = "";
 
-        }
-        res.status(200).json(obj)
+    if (uuidPattern.test(id)) {
+      obj = await Recipe.findOne({ where: { id } });
     }
 
-    catch (error) {
-        return res.status(501).send(error.message)
-
+    if (!obj) {
+      const { data } = await axios.get(`https://api.spoonacular.com/recipes/${id}/information${apiKey}${dietaFlag}`);
+      obj = RecipesTransformer(data);
     }
 
-}
-module.exports = getIdController
+    res.status(200).json(obj);
+  } catch (error) {
+    // Handle the error appropriately
+    console.error(error);
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
+module.exports = getIdController;
